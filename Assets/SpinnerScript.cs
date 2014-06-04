90860a6870bcb91498a44bc9ca1bb4fbd04b7972
Ross Bell <rossallenbell@gmail.com>
2014-06-03 21:04:05 -0500

    Added swirling background affect

---
+++ Assets/SpinnerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SpinnerScript : MonoBehaviour {
+
+	const float NormalRotation = 10f;
+	const float Variance = 2f;
+
+	private float rotation;
+
+	void Start () {
+		rotation = (NormalRotation - Variance) + (Variance * 2f * Random.value);
+	}
+	
+	void Update () {
+		transform.Rotate(Vector3.forward, rotation * Time.deltaTime);
+	}
+}
